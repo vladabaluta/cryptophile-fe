2b77566b6f78ab1f59235e01ecbb421c1aefd44f
vladabaluta2806 <vladabaluta2806@gmail.com>
2021-11-17 14:04:29 +0200

    cleared index.html, added a basic Navbar and navbar styling

--- src/components/Navbar/Navbar.styles.ts
+++ src/components/Navbar/Navbar.styles.ts
@@ -1,8 +1,33 @@
 import styled from 'styled-components';
 
-
 export const Navbar = styled.div`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
+	display: flex;
+	justify-content: space-between;
+	text-align: center;
+	padding: 0 2rem;
+	background-color: aliceblue;
+	height: 4rem;
+	align-items: center;
+	flex-wrap: wrap;
+`;
+
+export const Logo = styled.a`
+	color: #7b7fda;
+	text-decoration: none;
+	font-weight: 800;
+	font-size: 1.7rem;
+`;
+
+export const Menu = styled.div`
+	font-size: 1rem;
+	transition: all 0.3 ease-in;
+`;
+
+export const LinkMenuItems = styled.a`
+	padding: 1rem 2rem;
+	cursor: pointer;
+	text-align: center;
+	text-decoration: none;
+	transition: all 0.3s ease-in;
+	font-size: 0.9rem;
 `;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,18 @@
 import * as S from './Navbar.styles';
 
 const Navbar = (props: any) => {
-	return <Navbar>nav</Navbar>;
+	return (
+		<S.Navbar>
+			<S.Logo>Cryptophile</S.Logo>
+			<S.Menu>
+				<S.LinkMenuItems href="">Coins</S.LinkMenuItems>
+				<S.LinkMenuItems href="">Portofolio</S.LinkMenuItems>
+				<S.LinkMenuItems href="">Exchanges</S.LinkMenuItems>
+				<S.LinkMenuItems href="">Login</S.LinkMenuItems>
+				<S.LinkMenuItems href="">Register</S.LinkMenuItems>
+			</S.Menu>
+		</S.Navbar>
+	);
 };
 
 export default Navbar;
