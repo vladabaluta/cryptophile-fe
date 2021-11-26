c71f47a951641120b522983394d1331187d4e9cf
Chiciu Mihai <mihai99918@gmail.com>
2021-11-26 17:04:33 +0200

    added theme color to localStorage in ThemeContext

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -6,23 +6,28 @@ import IThemeContext from '../interfaces/IThemeContext';
 import variables from '../themes/variables';
 
 export const ThemeContext = createContext<IThemeContext>({
-	isDarkTheme: false,
-	toggleTheme: () => {},
+  isDarkTheme: false,
+  toggleTheme: () => {},
 });
 
 const ThemeProvider = ({ children }: any) => {
-	const [isDarkTheme, setIsDarkTheme] = useState(false);
-	const toggleTheme = () => setIsDarkTheme((prevState) => !prevState);
-
-	return (
-		<ThemeContext.Provider value={{ toggleTheme, isDarkTheme }}>
-			<StyledThemeProvider
-				theme={isDarkTheme ? Object.assign(darkTheme, variables) : Object.assign(lightTheme, variables)}
-			>
-				{children}
-			</StyledThemeProvider>
-		</ThemeContext.Provider>
-	);
+  const themeColor = JSON.parse(localStorage.getItem('themeColor') as 'string');
+  console.log('Theme Color', themeColor);
+  const [isDarkTheme, setIsDarkTheme] = useState(themeColor || false);
+  const toggleTheme = () =>
+    setIsDarkTheme((prevState: boolean) => {
+      localStorage.setItem('themeColor', JSON.stringify(!prevState));
+      return !prevState;
+    });
+  return (
+    <ThemeContext.Provider value={{ toggleTheme, isDarkTheme }}>
+      <StyledThemeProvider
+        theme={isDarkTheme ? Object.assign(darkTheme, variables) : Object.assign(lightTheme, variables)}
+      >
+        {children}
+      </StyledThemeProvider>
+    </ThemeContext.Provider>
+  );
 };
 
 export default ThemeProvider;
---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,9 @@
+import { createContext } from 'react';
+
+export const UserContext = createContext({});
+
+const UserProvider = ({ children }: any) => {
+  return <UserContext.Provider value={{}}>{children}</UserContext.Provider>;
+};
+
+export default UserProvider;
